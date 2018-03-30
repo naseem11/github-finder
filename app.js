8a35f57e4ab03685fb34033f8b26f5785e343d43
naseem11 <miannaseem11@gmail.com>
2018-03-31 00:41:25 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// init UI
+
+const ui=new UI;
+const key=new Key;
+
+// Add event listener to the username input 
+
+document.getElementById('userName').addEventListener('keyup',(e)=>{
+
+    // Dismiss alert if there is one
+
+ui.dismissAlert();
+
+const userName=e.target.value;
+
+
+if(userName!==''){
+
+    const gitHub=new Github(key);
+   
+    gitHub.getUser(userName)
+    .then((data)=>{
+
+        if(data.profile.message==='Not Found'){
+
+            ui.showAlert('User Not Found');
+
+        }else{
+                // show profile
+            ui.showProfile(data.profile);
+            ui.showRepos(data.repos);
+            console.log(data.repos);
+            
+        }
+    });
+
+}else{
+
+    //clear profile
+
+    ui.clearProfile();
+}
+
+});
---
+++ ui.js
@@ -0,0 +1,111 @@
+class UI{
+
+    constructor() {
+        this.profile=document.getElementById('profile');
+    }
+
+    showProfile(profile){
+        console.log(profile);
+        this.profile.innerHTML= `
+        
+        <div class="card card-body mb-3">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="${profile.avatar_url}" alt="Avatar" class="img-fluid rounded  border  ">
+                        <a href="${profile.html_url}" class="btn btn-primary btn-block my-3" target="_blank">Visit Profile</a>
+                    </div>
+                    <div class="col-md-9">
+                   <div class=" d-flex justify-content-around">
+                        <span class="badge badge-success ">Public Repos : ${profile.public_repos}</span>
+                        <span class="badge badge-primary ">Following : ${profile.following}</span>
+                        <span class="badge badge-warning ">Followers : ${profile.followers}</span>
+                        <span class="badge badge-info ">Gists : ${profile.public_gists}</span>
+                      
+
+                   </div>
+                   <br>
+                   <br>
+                    <ul class="list-group">
+                       <li class="list-group-item"><span class="font-weight-bold">Name :</span> ${profile.name}</li>
+                       <li class="list-group-item"><span class="font-weight-bold">Email :</span> ${profile.email}</li>
+                       <li class="list-group-item"><span class="font-weight-bold">Company :</span> ${profile.company}</li>
+                       <li class="list-group-item"><span class="font-weight-bold">Location :</span> ${profile.location}</li>
+                       <li class="list-group-item"><span class="font-weight-bold">Blog :</span><a href=" ${profile.blog}" target="_blank"> ${profile.blog}</a></li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+            <br><br>
+            <h3 class="page-heading my-3">Latest Repos</h3>
+            <div id="repos"></div>
+        `;
+
+        
+        
+
+            
+        
+
+        
+    }
+
+    showRepos(repos){
+        let output='';
+        repos.forEach((repo)=>{
+
+          output+=`
+             
+        <div class="card card-body mb-3">
+              <div class="row ">
+                    <div class="col-md-6">
+                        <a href="${repo.html_url}" target="_blank"> ${repo.name}</a>
+                    </div>
+                    <div class="col-md-6 ">
+                       <div class="d-flex justify-content-around">
+                            <span class="badge badge-success">Stars : ${repo.stargazers_count} </span>
+                            <span class="badge badge-warning"> Forks : ${repo.forks_count}</span>
+                            <span class="badge badge-info">Watchers : ${repo.watchers_count}</span>
+                       </div>
+                    </div>
+                </div>
+        </div>
+          
+          `
+
+        });
+
+      
+        document.getElementById('repos').innerHTML=output;
+    }
+
+
+        
+
+ 
+
+    showAlert(message){
+        this.dismissAlert();
+        const searchDiv=document.querySelector('.searchContainer');
+        const cardDiv=document.querySelector('.search');
+        const messageDiv=document.createElement('div');
+        messageDiv.textContent=message;
+        messageDiv.setAttribute('class','alert alert-danger');
+        searchDiv.insertBefore(messageDiv,cardDiv);
+
+    }
+
+    dismissAlert(){
+
+        const messageDiv=document.querySelector('.alert');
+        if(messageDiv){
+
+            messageDiv.remove();
+        }
+    }
+
+    clearProfile(){
+
+        this.profile.innerHTML='';
+
+    }
+}
